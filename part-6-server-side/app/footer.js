6866d0b7143a3ae4ddff9603dfcd510a3aff7045
Your Name <you@example.com>
2020-03-04 13:15:22 +0300

    changed files

--- part-6-server-side/app/footer.js
+++
@@ -1,7 +0,0 @@
-
-const footer = {
-    height: 30,
-    template:"The software is provided by <a href='#'>webix.com</a>. All rights reserved (c)",
-    css:"bottom-link"
-}
-
--- part-6-server-side/app/header.js
+++
@@ -1,8 +0,0 @@
-const header = {
-    view:"toolbar",
-    css:"webix_dark tool_flex",
-    cols:[
-        { view:"label", label:"My app", autowidth:true},
-        {}
-    ]
-}
